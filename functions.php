5f8bb6770e6cbe083eb27e96d2c358e35abc75ce
charlinems <charlinestm@gmail.com>
2018-10-15 20:46:29 -0300

    Commitinicial. Adicionado README.md. Adicionado functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,21 @@
 <?php
 
-	functions umaFuncao(){
-		echo "Uma função que imprime".php_EOL;
+	function umaFuncao(){
+		echo "Uma função que imprime".PHP_EOL;
 	}
+
+	
+	function outraFuncao($nome){
+		echo "Olá,{$nome}!".PHP_EOL;
+	}
+	
+	
+	function outraFuncao($nome){
+		echo "Olá, {nome}'.Eu sou uma funcao!".PHP_EOL;
+
+	}
+
+	function novaFuncao($msg){
+		echo "Sou uma função que recebe:{$msg} ".PHP_EOL;
 
 ?>
